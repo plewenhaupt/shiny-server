39529de50ccb4dea90e8086562c6178f5098e508
plewenhaupt <plewenhaupt@gmail.com>
2018-02-21 08:36:40 +0000

    New plot

--- Sweden_stats/server.R
+++ Sweden_stats/server.R
@@ -10,14 +10,8 @@ shinyServer(function(input, output) {
   load("populationdf.Rdata")
    
   output$popPlot <- renderPlot({
-    popPlot <- ggplot(populationdf, aes(x=Year)) 
-    popPlot <- popPlot + geom_line(aes(y=Men, group=1), color="blue")
-    popPlot <- popPlot + geom_line(aes(y=Women, group=1), color="red")
-    popPlot <- popPlot + geom_line(aes(y=Total, group=1), color="green")
-    popPlot <- popPlot + scale_y_continuous(labels = comma)
-    popPlot <- popPlot + theme(axis.title.y=element_blank(), axis.text.y=element_text(size = 7), plot.margin = margin(10, 10, 20, 25))
+    ggplot(populationdf, aes(x=Year)) + geom_line(aes(y=Men, group=1), color="blue") + geom_line(aes(y=Women, group=1), color="red") + geom_line(aes(y=Total, group=1), color="green") + scale_y_continuous(labels = comma) + theme(axis.title.y=element_blank(), axis.text.y=element_text(size = 7), plot.margin = margin(10, 10, 20, 25))
     #popPlot <- ggplotly(popPlot, tooltip = c("y", "x"))
-    return(popPlot)
   })
   
 })
