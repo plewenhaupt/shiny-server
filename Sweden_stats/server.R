d4139ef92c0e22e9c7397fb01713b16542d792bc
plewenhaupt <plewenhaupt@gmail.com>
2018-02-21 08:27:29 +0000

    Testing on template.

--- Sweden_stats/server.R
+++ Sweden_stats/server.R
@@ -9,15 +9,15 @@ library(plotly)
 shinyServer(function(input, output) {
   load("populationdf.Rdata")
    
-  output$popPlot <- renderPlotly({
+  output$popPlot <- renderPlot({
     popPlot <- ggplot(populationdf, aes(x=Year)) 
     popPlot <- popPlot + geom_line(aes(y=Men, group=1), color="blue")
     popPlot <- popPlot + geom_line(aes(y=Women, group=1), color="red")
     popPlot <- popPlot + geom_line(aes(y=Total, group=1), color="green")
     popPlot <- popPlot + scale_y_continuous(labels = comma)
     popPlot <- popPlot + theme(axis.title.y=element_blank(), axis.text.y=element_text(size = 7), plot.margin = margin(10, 10, 20, 25))
-    popPlot <- ggplotly(popPlot, tooltip = c("y", "x"))
-    print(popPlot)
+    #popPlot <- ggplotly(popPlot, tooltip = c("y", "x"))
+    return(popPlot)
   })
   
 })
--- Sweden_stats/ui.R
+++ Sweden_stats/ui.R
@@ -5,12 +5,20 @@ shinyUI(fluidPage(
   
   # Application title
   titlePanel("Visualizations of Swedish statistics - Proof of concept 1"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("bins",
+                  "Number of bins:",
+                  min = 1,
+                  max = 50,
+                  value = 30)
+    ),
 
     mainPanel(
-      tabsetPanel(
-        tabPanel("Population", plotlyOutput("popPlot")),
-        tabPanel("Summary")
+    plotOutput("popPlot")
       )
     )
   )
 )
+
